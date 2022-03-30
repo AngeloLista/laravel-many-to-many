010b3cee207fceec46a040db7df620eb5bcf2281
AngeloLista <abstractplayer@gmail.com>
2022-03-31 00:38:25 +0200

    added timestamps in post_tag table

--- database/migrations/2022_03_30_222206_create_post_tag_table.php
+++ database/migrations/2022_03_30_222206_create_post_tag_table.php
@@ -20,6 +20,8 @@ class CreatePostTagTable extends Migration
 
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+
+            $table->timestamps();
         });
     }
 
